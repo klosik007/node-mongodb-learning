ea9e1300a94f521ef987e910c106c13490deac14
Przemek <pklos1992@gmail.com>
2018-12-06 21:46:10 +0100

    queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,39 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = "5c0980199e737d117c816172";
+
+// if(!ObjectID.isValid(id)){
+//     console.log('ID not valid');
+// }
+// Todo.find({
+//     _id: id
+// }).then((todo2) =>{
+//     console.log('Todo2', todo2);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo2) =>{
+//     console.log('Todo2', todo2);
+// });
+
+// Todo.findById(id).then((todo2) =>{
+//     if (!todo2) return console.log('Id not found');
+//     console.log('Todo2 by id', todo2);
+// }).catch((e) =>console.log(e));
+
+var id = '5be09787571e501468990633';
+
+// if(!ObjectID.isValid(id)){
+//     console.log('ID not valid');
+// }
+
+User.findById(id).then((user) =>{
+    if (!user) return console.log('Id not found');
+    console.log('User by id', user);
+}).catch((e) => console.log(e));
+
+
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,15 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [
+    {text: 'First to do'},
+    {text: 'Second to do'}
+];
+
 beforeEach((done) =>{
     Todo.remove({}).then(() =>{
-        done();
-    });
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todo2', () =>{
@@ -27,7 +32,7 @@ describe('POST /todo2', () =>{
                 }
 
                 Todo.find().then((todo2) =>{
-                    expect(todo2.length).toBe(1);
+                    expect(todo2.length).toBe(3);
                     expect(todo2[0].text).toBe(text);
                     done();
                 }).catch((e) => done(e));
@@ -45,9 +50,21 @@ describe('POST /todo2', () =>{
                 }
 
                 Todo.find().then((todo2)=>{
-                    expect(todo2.length).toBe(0);
+                    expect(todo2.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
+    });
+});
+
+describe('GET /todos', () =>{
+    it('should get all todos', (done) =>{
+        request(app)
+            .get('/todo2')
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todo2.length).toBe(2);
+            })
+            .end(done);
     });
 });
