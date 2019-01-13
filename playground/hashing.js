98292c9c2c75789367b9b99ef090432dc60dc3b7
Przemek <pklos1992@gmail.com>
2019-01-13 20:50:15 +0100

    added password hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,6 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcryptjs = require('bcryptjs');
 
 var data = {
     id: 10
@@ -10,6 +11,19 @@ console.log(token);
 
 var decoded = jwt.verify(token, 'fewf');
 console.log(decoded);
+
+var password = '1124ffg';
+
+bcryptjs.genSalt(10, (err, salt) =>{
+    bcryptjs.hash(password, salt, (err, hash) =>{
+        console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$WVvMYsiU/nUWK6rVWdPL/u2n7NBlOi3SBZLaMYHDwuU3BSn8WRuti';
+bcryptjs.compare(password, hashedPassword, (err, res) =>{
+    console.log(res);
+});
 // var message = "fswffwwgwgwg";
 // var hash = SHA256(message).toString();
 
--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -2,18 +2,18 @@ var {User} = require('./../models/user');
 
 var authenticate = (req, res, next) => {
     //var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzM2M2Q5YmM5MzFjMjI5ZTRjN2RiZGIiLCJhY2Nlc3MiOiJhdXRoIiwiaWF0IjoxNTQ3MDU4NTg3fQ.IIzDCv_sgkZamR02q9PQ5icYZ193poIEfZL7V2BLeQo';
-    var token = req.header('x-auth');
-    console.log(token);
+    var token = req.header('x-auth');//still dont know why is this undefined when calling
+    console.log(token); 
     
     User.findByToken(token).then((user) =>{
         if (!user){
             return Promise.reject();
         }
 
-        req.user = user;
-        req.token = token;
-        next(); //need it cause in app.get /users/me the res.send will not execute otherwise
-        //res.send(user);
+        req.user = user; //by this we have specified user saved in req.user call, 
+                         //it provides us to use right user in app.get /users/me 
+        req.token = token;//and as above with token
+        next(); //need it cause otherwise in app.get /users/me the res.send will not execute 
     }).catch((e) =>{
         res.status(401).send();
     });
