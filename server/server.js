b122d315f3b7c96cb4b06441855079c8bc1ffbe8
Przemyslaw Klos <pklos1992@gmail.com>
2019-01-17 23:54:08 +0100

    added POST /users/login

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var {authenticate} = require('./middleware/authenticate');
+var bcrypt = require('bcryptjs');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -150,6 +151,18 @@ app.post('/users', (req, res) =>{
 
 app.get('/users/me', authenticate, (req, res) =>{
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res)=>{
+    var body = _.pick(req.body, ['email', 'password']);
+    
+    User.findByCredentials(body.email, body.password).then((user) =>{
+        return user.generateAuthToken().then((token) =>{
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((e) =>{
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () =>{
